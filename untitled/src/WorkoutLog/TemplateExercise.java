c12a41ce8174062f1bb28315b5a20eab76098933
MatthewJohnKing <137497118+MatthewJohnKing@users.noreply.github.com>
2025-05-14 18:42:44 +0100

    TemplateExercise comments
    Instructions on building an exercise added

--- untitled/src/WorkoutLog/TemplateExercise.java
+++ untitled/src/WorkoutLog/TemplateExercise.java
@@ -3,6 +3,12 @@ package WorkoutLog;
 import static java.lang.Math.sqrt;
 
 public class TemplateExercise  extends Exercise{
+
+    // The template Exercise class is designed to be updated with the specific values for the workout.
+    // When constructing an exercise subclass first change the name of the constructors and the class itself.
+
+    // Change the name in both constructors and the ReferenceScore(Number) so that it is accurate.
+
     public TemplateExercise() {
         super();
         super.name="This is not an exercise!";
@@ -13,11 +19,14 @@ public class TemplateExercise  extends Exercise{
         super.setReferenceScore(1);
     }
 
+    // Does not need changing
     @Override
     public double exerciseScore(){
         return this.exerciseScore(false);
     }
 
+    // Select correct form for the calculation of the score of the workout. Or add your own :-)
+    @Override
     public double exerciseScore(Boolean setScore){
         final Double[] score = {0.0};
         // super.getExerciseSets().forEach( dat ->  { score[0] += dat[0]*dat[1];});
